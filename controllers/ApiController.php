c41275475728bb91543921b3cb4e70cbac8d57c5
GamerWindow <p.r.kaiser@gamerwindow.de>
2019-11-13 13:52:05 +0100

    Node-RED Dashboard hinzugefügt

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -4,10 +4,31 @@ namespace app\controllers;
 
 use app\models\Message;
 use Yii;
-use yii\rest\Controller;
+use yii\web\Controller;
 
 class ApiController extends Controller
 {
+    public $enableCsrfValidation = false;
+
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+
+            // For cross-domain AJAX request
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    // restrict access to domains:
+                    'Origin' => ["*"],
+                    'Access-Control-Allow-Origin' => true,
+                    // 'Access-Control-Request-Method' => ['POST'],
+                    // 'Access-Control-Allow-Credentials' => true,
+                    // 'Access-Control-Max-Age' => 3600, // Cache (seconds)
+                ],
+            ],
+
+        ]);
+    }
 
     public function actionMessages()
     {
@@ -48,5 +69,41 @@ class ApiController extends Controller
             $ok = false;
         }
         return $this->asJson(["succes" => $ok]);
+    }
+
+    public function actionStats()
+    {
+        $count = Yii::$app->db->createCommand('SELECT COUNT(*) FROM message')
+            ->queryOne();
+        $neu = Yii::$app->db->createCommand('SELECT max(datum) FROM message')
+            ->queryOne();
+
+        $alt = Yii::$app->db->createCommand('SELECT min(datum) FROM message')
+            ->queryOne();
+        echo '<p> Es sind bereits ' . $count . ' Nachrichten.</p>';
+        echo '<p>Die neueste Nachricht ist vom: ' . $neu . '</p>';
+        echo '<p>Die älteste Nachricht ist vom: ' . $alt . '</p>';
+
+    }
+
+    public function actionDeletepost($datum)
+    {
+
+        $data = $datum;
+        Yii::trace("datum=" . $data);
+        $numDeleted = Message::deleteAll(['datum' => $datum]);
+        return $this->asJson(["succes" => $numDeleted >= 1]);
+
+        /*$data = Message::find()->where('datum =' . $datum)->all();
+    foreach ($data as $dat) {
+    $dat->delete();
+    }*/
+    }
+
+    public function actionHello()
+    {
+
+        return $this->asJson(["text" => "hello"]);
+
     }
 }
